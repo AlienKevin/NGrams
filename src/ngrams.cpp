4715275c69ebbdb9a342b46f722aa5b53ca6d045
Kevin Li <kevinli020508@gmail.com>
2019-07-07 15:24:10 -0700

    Add printIntroduction function

--- src/ngrams.cpp
+++ src/ngrams.cpp
@@ -2,7 +2,18 @@
 #include "console.h"
 using namespace std;
 
+void printIntroduction();
+
 int main() {
+    printIntroduction();
     cout << "Exiting." << endl;
     return 0;
+}
+
+void printIntroduction() {
+    cout << "Welcome to CS 106B/X Random Writer ('N-Grams')!" << endl;
+    cout << "This program generates random text based on a document." << endl;
+    cout << "Give me an input file and an 'N' value for groups" << endl;
+    cout << "of words, and I'll create random text for you." << endl;
+    cout << endl;
 }
