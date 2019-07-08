f5dc4bafcad501cbad69efaadc48659c98713e19
Kevin Li <kevinli020508@gmail.com>
2019-07-07 19:53:06 -0700

    Can create map of prefixes to suffixes

--- src/ngrams.cpp
+++ src/ngrams.cpp
@@ -8,11 +8,11 @@
 using namespace std;
 
 void printIntroduction();
-void promptForFile(Map<Queue<string>, vector<string> >& map);
+void promptForFile(Map<Queue<string>, Vector<string> >& map);
 
 int main() {
     printIntroduction();
-    Map<Queue<string>, vector<string> > map;
+    Map<Queue<string>, Vector<string> > map;
     promptForFile(map);
     cout << "Exiting." << endl;
     return 0;
@@ -26,7 +26,7 @@ void printIntroduction() {
     cout << endl;
 }
 
-void promptForFile(Map<Queue<string>, vector<string> >& map) {
+void promptForFile(Map<Queue<string>, Vector<string> >& map) {
     ifstream file;
     promptUserForFile(file, "Input file name? ");
     int n = 0;
@@ -37,7 +37,44 @@ void promptForFile(Map<Queue<string>, vector<string> >& map) {
         }
     }
     string word;
+    int totalWords = 0;
     while (file >> word) {
-
+        totalWords ++;
     }
+
+    // point istream back to beginning of file
+    // https://bytes.com/topic/c/answers/881293-reading-file-multiple-times
+    file.clear();
+    file.seekg(0, ios::beg);
+
+    Queue<string> window;
+    Queue<string> prefixes;
+    int wordCount = 0;
+    Vector<string> frontWordList;
+    Vector<string> backWordList;
+    while (file >> word) {
+        if (wordCount < n) {
+            frontWordList.add(word);
+        } else if (totalWords - wordCount <= n) {
+            backWordList.add(word);
+        }
+        if (window.size() == n) {
+            prefixes = window;
+            map[prefixes].add(word);
+            window.dequeue();
+        }
+        window.enqueue(word);
+        wordCount ++;
+    }
+    Vector<string> edgeWordList = backWordList + frontWordList;
+    cout << edgeWordList << endl;
+    for (int i = 0; i < edgeWordList.size(); i++) {
+        if (window.size() == n) {
+            prefixes = window;
+            map[prefixes].add(word);
+            window.dequeue();
+        }
+        window.enqueue(word);
+    }
+    cout << map << endl;
 }
